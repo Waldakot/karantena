33470066da5a6282254b2ced69901d494577b60a
Waldakot <48911523+Waldakot@users.noreply.github.com>
2020-03-22 18:24:25 +0100

    počítání sítí s dvěmi maskami

--- php.php
+++ php.php
@@ -7,7 +7,7 @@
 
 <body>
     <form action="php.php" method="post">
-        <h1>Počítání IP Adres</h1>
+        <h1>Počítání IP Adres (pouze s dvěmi podsítěmi)</h1>
         <p>Vložte IP adresu</p>
         <input type="number" class="input" placeholder="192" id="d1" name="o1" min="0" max="255" value="" required="">
         <input type="number" class="input" placeholder="168" id="d2" name="o2" min="0" max="255" value="" required="">
@@ -434,7 +434,378 @@ echo "<br>";
 echo "<big><big>Host:</big></big>";
 echo $host;
 
+$maska3;
+
+switch($prefix){
+case 1:
+case 24:
+    $maska3 = "255.255.255.128";
+    break;
+    case 25:
+    $maska3 = "255.255.255.192";
+    break;
+    case 26:
+    $maska3 = "255.255.255.224";
+    break;
+    case 27:
+    $maska3 = "255.255.255.240";
+    break;
+    case 28:
+    $maska3 = "255.255.255.248";
+    break;
+    case 29:
+    $maska3 = "255.255.255.252";
+    break;
+    case 30:
+    $maska3 = "255.255.255.252";
+    break;
+    case 31:
+    $maska3 = "255.255.254.";
+    break;
+    case 32:
+    $maska3 = "255.255.255.255";
+    break;
+    }
+
+echo "<h2>1.podsíť </h2>";
+
+echo "<b>maska </b>";
+echo "<br>";
+echo "$maska3";
+echo "<br>";
+
+
+
+echo "<b>first host </b>";
+echo "<br>";
+echo $d1;
+echo ".";
+echo $d2;
+echo ".";
+echo $d3;
+echo ".";   
+echo "001";
+echo "<br>";
+
+
+$lasthost;
+switch($prefix){
+    case 1:
+    case 24:
+        $lasthost = "$d1.$d2.$d3.126";
+        break;
+        case 25:
+        $lasthost = "$d1.$d2.$d3.062";
+        break;
+        case 26:
+        $lasthost = "$d1.$d2.$d3.030";
+        break;
+        case 27:
+        $lasthost = "$d1.$d2.$d3.014";
+        break;
+        case 28:
+        $lasthost = "$d1.$d2.$d3.006";
+        break;
+        case 29:
+        $lasthost = "$d1.$d2.$d3.002";
+        break;
+        case 30:
+        $lasthost = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        case 31:
+        $lasthost = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        case 32:
+        $lasthost = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        }
+    
+        echo "<b>Last host  </b>";
+        echo "<br>";
+        echo "$lasthost";
+      
+        
+        $broadcast;
+        switch($prefix){
+            case 1:
+            case 24:
+                $broadcast = "$d1.$d2.$d3.127";
+                break;
+                case 25:
+                $broadcast = "$d1.$d2.$d3.063";
+                break;
+                case 26:
+                $broadcast = "$d1.$d2.$d3.031";
+                break;
+                case 27:
+                $broadcast = "$d1.$d2.$d3.015";
+                break;
+                case 28:
+                $broadcast = "$d1.$d2.$d3.007";
+                break;
+                case 29:
+                $broadcast = "$d1.$d2.$d3.003";
+                break;
+                case 30:
+                $broadcast = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                break;
+                case 31:
+                $broadcast = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                break;
+                case 32:
+                $broadcast = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                break;
+                }
+
+                echo "<br>";
+                echo "<b>broadcast  </b>";
+                echo "<br>";
+                echo "$broadcast";
+              
+                $host5;
+                switch($prefix){
+                    case 1:
+                    case 24:
+                        $host5 = "126";
+                        break;
+                        case 25:
+                            $host5 = "62";
+                        break;
+                        case 26:
+                            $host5 = "30";
+                        break;
+                        case 27:
+                            $host5 = "14";
+                        break;
+                        case 28:
+                            $host5 = "6";
+                        break;
+                        case 29:
+                            $host5 = "2";
+                        break;
+                        case 30:
+                            $host5 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                        break;
+                        case 31:
+                            $host5 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                        break;
+                        case 32:
+                            $host5 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                        break;
+                        }
+
+                        echo "<br>";
+                        echo "<b>host  </b>";
+                        echo "<br>";
+                        echo "$host5";
+
+
+
+
+
+
+
+
+
+
+
+
+                        $maska4;
+
+switch($prefix){
+case 1:
+case 24:
+    $maska4 = "255.255.255.128";
+    break;
+    case 25:
+    $maska4 = "255.255.255.192";
+    break;
+    case 26:
+    $maska4 = "255.255.255.224";
+    break;
+    case 27:
+    $maska4 = "255.255.255.240";
+    break;
+    case 28:
+    $maska4 = "255.255.255.248";
+    break;
+    case 29:
+    $maska4 = "255.255.255.252";
+    break;
+    case 30:
+    $maska4 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+    break;
+    case 31:
+    $maska4 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+    break;
+    case 32:
+    $maska4 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+    break;
+    }
+
+echo "<h2>2.podsíť </h2>";
+
+echo "<b>maska </b>";
+echo "<br>";
+echo "$maska4";
+echo "<br>";
+
+
+$firsthost1;
+switch($prefix){
+    case 1:
+    case 24:
+        $firsthost1 = "$d1.$d2.$d3.129";
+        break;
+        case 25:
+            $firsthost1 = "$d1.$d2.$d3.065";
+        break;
+        case 26:
+            $firsthost1 = "$d1.$d2.$d3.033";
+        break;
+        case 27:
+            $firsthost1 = "$d1.$d2.$d3.017";
+        break;
+        case 28:
+            $firsthost1 = "$d1.$d2.$d3.009";
+        break;
+        case 29:
+            $firsthost1 = "$d1.$d2.$d3.005";
+        break;
+        case 30:
+            $firsthost1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        case 31:
+            $firsthost1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        case 32:
+            $firsthost1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        }
+
+        echo "<b>first host </b>";
+        echo "<br>";
+        echo "$firsthost1";
+        echo "<br>";
+
+$lasthost1;
+switch($prefix){
+    case 1:
+    case 24:
+        $lasthost1 = "$d1.$d2.$d3.254";
+        break;
+        case 25:
+        $lasthost1 = "$d1.$d2.$d3.126";
+        break;
+        case 26:
+        $lasthost1 = "$d1.$d2.$d3.062";
+        break;
+        case 27:
+        $lasthost1 = "$d1.$d2.$d3.030";
+        break;
+        case 28:
+        $lasthost1 = "$d1.$d2.$d3.014";
+        break;
+        case 29:
+        $lasthost1 = "$d1.$d2.$d3.006";
+        break;
+        case 30:
+        $lasthost1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        case 31:
+        $lasthost1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        case 32:
+        $lasthost1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+        break;
+        }
+    
+        echo "<b>Last host  </b>";
+        echo "<br>";
+        echo "$lasthost1";
+      
+        
+        $broadcast1;
+        switch($prefix){
+            case 1:
+            case 24:
+                $broadcast1 = "$d1.$d2.$d3.255";
+                break;
+                case 25:
+                $broadcast1 = "$d1.$d2.$d3.127";
+                break;
+                case 26:
+                $broadcast1 = "$d1.$d2.$d3.063";
+                break;
+                case 27:
+                $broadcast1 = "$d1.$d2.$d3.031";
+                break;
+                case 28:
+                $broadcast1 = "$d1.$d2.$d3.015";
+                break;
+                case 29:
+                $broadcast1 = "$d1.$d2.$d3.007";
+                break;
+                case 30:
+                $broadcast1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                break;
+                case 31:
+                $broadcast1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                break;
+                case 32:
+                $broadcast1 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                break;
+                }
+
+                echo "<br>";
+                echo "<b>broadcast  </b>";
+                echo "<br>";
+                echo "$broadcast1";
+              
+                $host5;
+                switch($prefix){
+                    case 1:
+                    case 24:
+                        $host5 = "126";
+                        break;
+                        case 25:
+                            $host5 = "62";
+                        break;
+                        case 26:
+                            $host5 = "30";
+                        break;
+                        case 27:
+                            $host5 = "14";
+                        break;
+                        case 28:
+                            $host5 = "6";
+                        break;
+                        case 29:
+                            $host5 = "2";
+                        break;
+                        case 30:
+                            $host5 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                        break;
+                        case 31:
+                            $host5 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                        break;
+                        case 32:
+                            $host5 = "tolik podsíti nelze v takovém rozsahu vytvořit";
+                        break;
+                        }
+
+                        echo "<br>";
+                        echo "<b>host  </b>";
+                        echo "<br>";
+                        echo "$host5";
+
+
+
+
+
     ?>
+
+
 
 
 
